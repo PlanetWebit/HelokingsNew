6cbbea966aa18de62cc15938b9ddbd1d6c6e153f
User <rishabhkumar@planetwebit.com>
2023-03-04 14:21:09 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/planet/com/helokings/Fragment/MeFragment.java
+++ app/src/main/java/planet/com/helokings/Fragment/MeFragment.java
@@ -1,5 +1,6 @@
 package planet.com.helokings.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,16 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import planet.com.helokings.Activity.StoreActivity;
 import planet.com.helokings.R;
+import planet.com.helokings.databinding.FragmentMeBinding;
 
 
 public class MeFragment extends Fragment {
+    FragmentMeBinding meBinding;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+
+        meBinding = FragmentMeBinding.inflate(inflater, container, false);
+        View root = meBinding.getRoot();
+
+
+        meBinding.layStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), StoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
     }
 }
